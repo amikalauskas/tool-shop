02be0a594b40f4884002d08a20bae0fd8b0f5b31
amikalauskas <andriusminadvirati@gmail.com>
2022-10-27 19:28:45 +0300

    fix: typo fix

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -39,7 +39,7 @@ const CartScreen = () => {
           <h1> Shopping Cart</h1>
           {cartItems.length === 0 ? (
             <Message>
-              Your cart is mepty <Link to="/">Go Back</Link>
+              Your cart is empty <Link to="/">Go Back</Link>
             </Message>
           ) : (
             <ListGroup variant="flush">
